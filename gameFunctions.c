7d4fb25376c8f294a06bf89f7e14a16e39071e81
qwedsaz <46765114+qwedsaz@users.noreply.github.com>
2019-03-11 23:40:36 -0500

    Update gameFunctions.c

--- gameFunctions.c
+++ gameFunctions.c
@@ -4,6 +4,33 @@
 
 #include "gameFunctions.h"
 
+typedef int bool;
+
+/*
+ * Print the string into underscores with same length
+ */
+void initializeBlankString(int, char revealedLetters[]);
+
+/*
+ * Print the blank underscores for the underlying word with spaces in between
+ */
+void printWithSpaces(char revealedLetters[]);
+
+/*
+ * Reveal the position of correct characters
+ */
+bool revealGuessedLetter(char word[], char revealedLetters[], char guess);
+
+/*
+ * Determine if the words are match
+ */
+bool checkGuess(char word[], char revealedLetters[]);
+
+/*
+ * Start the game with all required functions inside
+ */
+void startGame(char word[]);
+
 initializeBlankString(int wordLen, char revealedLetters[]) {
   int i;
   for (i = 0; i < wordLen; ++i) {
