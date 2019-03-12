442580073191ede52e6533df5816bb85ac8e69c5
qwedsaz <46765114+qwedsaz@users.noreply.github.com>
2019-03-12 00:05:19 -0500

    Update gameFunctions.h

--- gameFunctions.h
+++ gameFunctions.h
@@ -3,12 +3,12 @@ typedef int bool;
 /*
  * Print the string into underscores with same length
  */
-void initializeBlankString(int, char revealedLetters[]);
+void initializeBlankString(int, char *revealedLetters);
 
 /*
  * Print the blank underscores for the underlying word with spaces in between
  */
-void printWithSpaces(char revealedLetters[]);
+void printWithSpaces(char *revealedLetters);
 
 /*
  * Reveal the position of correct characters
